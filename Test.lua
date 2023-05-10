b70592171703430f6fb54f1c078436196000607c
Milleu830 <129146211+Milleu830@users.noreply.github.com>
2023-05-10 20:55:25 +0700

    Add files via upload

---
+++ Test.lua
@@ -0,0 +1,6 @@
+Key = "Key_10282928372"-- Put here key or raw link on key. Example: "https://1.kelprepl.repl.co/example", "Key_13904293482"
+Link = "none"-- Put here link. Example: "https://1.kelprepl.repl.co/example", "https://bit.ly/example"
+Script = "https://raw.githubusercontent.com/Milleu830/123/main/chill%20hub.lua"-- Put here your script link. Example: "https://pastebin.com/raw/example"
+
+--! DON'T TOUCH ANYTHING BELOW !--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/FreeKeySystem/main/Lib.lua"))().Sus(Link,Script,Key)
